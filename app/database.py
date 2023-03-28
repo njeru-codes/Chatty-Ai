182e2513aeccdfa3dea63c2c54b8bce8b9a9e23d
NJERU MTWAITI <bernardnjerumtwaiti@gmail.com>
2023-03-28 11:42:36 +0000

    bot code version 0.0.1

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os, pymongo 
 from datetime import datetime
 
-#load_dotenv()
+load_dotenv()
 
 async def create_connection():
     try:
